2ba9d340d5aff8aa74e56596fe7775c11c7e621e
Maciej Kosmela <alternatywnykosmos@gmail.com>
2021-08-18 15:21:14 +0200

    create methods to hide and show markers and next I working at filter method to partial markers hide

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,65 +25,11 @@ export class AppComponent implements OnInit, OnDestroy{
     select: 'Please select category',
   }
   
-  // Markers
-
-
+  
   objects: Object[]=[] ;
   private subscription = new Subscription();
   constructor(private http: VehicleService,public dialog: MatDialog) { }
 
-
-  // clickedMarker(marker:Marker,i:number) {
-  //   console.log('Clicked Marker: ' +marker+ 'at index' +i);  
-  // };
-  // mapClicked($event: any) {
-  //   const newMarker = {
-  //     name: 'Untitle',
-  //     lat: $event.coords.lat,
-  //     lng: $event.coords.lng,
-  //     draggable: false
-  //   }
-  //   this.markers.push(newMarker)
-  // };
-
-  // markerDragEnd(marker: any, $event: any) {
-  //   console.log('dragEnd', marker, $event);
-    
-
-  //   const updMarker = {
-  //     name: marker.name,
-  //     lat: parseFloat(marker.lat),
-  //     lng: parseFloat(marker.lng),
-  //     draggable:false
-  //   }
-
-  //   const newLat = $event.coords.lat;
-  //   const newLng = $event.coords.lng;
-
-  // };
-
-  // Function to filter markers by category
- 
-
-// filterMarkers(value) {
-//   for (this.markers) {
-//     // markers = gmarkers[i];
-//     // // If is same category or category not picked
-//     if (this.markers.category == value || value.length === 0) {
-//       this.markers.setVisible(false);
-//     }
-//     // Categories don't match 
-//     else {
-//       this.markers.setVisible(true);
-//     }
-//   }
-// }
-showSelect() {
-   console.log(this.field_data.select);
-   
-}
-
-  
   ngOnInit(): any {
     const sub1 = this.http.getObjects().subscribe(objs => {
       this.objects = objs['objects'];
@@ -92,16 +38,44 @@ showSelect() {
       () => console.log('Complite')
     );
     this.subscription.add(sub1);
+};
+
+
+  showAllCars(): void {
+    
+    const sub2 = this.http.getObjects().subscribe(objs => {
+      this.objects = objs['objects'];
+      console.log(this.objects);
+    }, error => console.error(error),
+      () => console.log('Complite')
+    );
+    this.subscription.add(sub2);
+
   };
 
-  openDialog(obj: any,i:any) {
+  hideTrucks(): void {
+    if (this.objects.filter(obj=> obj.type == 'TRUCK')) {
+      console.log(this.objects);
+      
+    }
+    
+  };
+
+  hideCars(): void {
+    this.objects =[];
+  };
+
+
+
+openDialog(obj: any,i:any) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.data = [{ data: obj },{indexobj:i}];
 
     this.dialog.open(DesctriptionComponent, dialogConfig);
-  };
+};
 
-  ngOnDestroy() {
+ngOnDestroy() {
     this.subscription.unsubscribe();
   };
+  
 }
